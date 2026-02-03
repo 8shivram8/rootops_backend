8c66ed306fe8f2609cc8d452489710bc2be183ec
shivram <abhiram@gmail.com>
2026-02-03 23:41:03 +0530

    replace nodemailer with resend email api

--- config/email.js
+++ config/email.js
@@ -1,26 +1,14 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async (to, subject, htmlContent) => {
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      html: htmlContent,
-    });
-    console.log('Email sent successfully');
-  } catch (error) {
-    console.error('Email sending error:', error);
-    throw error;
-  }
+const sendEmail = async (to, subject, html) => {
+  return resend.emails.send({
+    from: "RootOps <onboarding@resend.dev>",
+    to,
+    subject,
+    html,
+  });
 };
 
 module.exports = sendEmail;
